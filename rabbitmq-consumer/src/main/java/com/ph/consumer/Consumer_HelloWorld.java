644e51add9440c7549d1bcab841a255d64bde17c
panhao <phnobugs@gmail.com>
2021-06-30 15:13:00 +0800

    更新rabbitmq测试用例：增加生产者和消费者。 更新：最简单的工作模式：一个生产者对应一个消费者。 更新：完善消费者获取信息，重写handleDelivery方法，将队列中获取到的信息打印出来。

--- rabbitmq-consumer/src/main/java/com/ph/consumer/Consumer_HelloWorld.java
+++ rabbitmq-consumer/src/main/java/com/ph/consumer/Consumer_HelloWorld.java
@@ -1,8 +1,6 @@
 package com.ph.consumer;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -41,5 +39,36 @@ public class Consumer_HelloWorld {
          */
         //注意如果没有一个名叫helloworld的队列，则会创建该队列，如果有则不会创建。
         channel.queueDeclare("helloworld",true,false,false,null);
+
+        //接收消息
+        /***
+         basicConsume(String queue, boolean autoAck, Consumer callback)
+         参数:
+            1、queue:队列名称
+            2、autoAck：是否自动确认
+            3、callback：回调对象
+         */
+        Consumer consumer=new DefaultConsumer(channel){
+            /**
+             回调方法。当收到消息后，会自动执行该方法。
+                 1、consumerTag:标识
+                 2、envelope：获取一些信息。比如交换机的信息，路由key。。。
+                 3、properties：配置信息
+                 4、body：数据
+             */
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("consumerTag:"+consumerTag);
+                System.out.println("envelope"+envelope.getExchange());
+                System.out.println("RoutingKey:"+envelope.getRoutingKey());
+                System.out.println("properties:"+properties);
+                System.out.println("body:"+new String(body));
+            }
+        };
+        channel.basicConsume("helloworld",true,consumer);
+        //要关闭资源吗？当然是不要，因为消费者相当于一个监听程序，当消息来了之后，我再接受它，所以要一直保持连接的状态。
+
+
+
     }
 }
